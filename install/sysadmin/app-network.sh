49f409ec277ee5e6108d41558cc9e4ddaa4c928c
MuhammadAKJ <Muhammadakj11@gmail.com>
2024-06-24 21:21:16 +0100

    network management tools

--- install/sysadmin/app-network.sh
+++ install/sysadmin/app-network.sh
@@ -2,9 +2,12 @@
 
 # Wireshark
 # A network protocol analyzer that allows administrators to capture and interactively browse the traffic running on a computer network.
+sudo apt install wireshark -y
 
 # nmap
 # A network scanning tool that provides discovery and security auditing capabilities to identify devices and services on a network.
+sudo apt-get install nmap -y
 
 # Netcat
 # A versatile networking utility which reads and writes data across network connections, aiding in debugging and network service scripting.
+sudo apt-get install netcat
