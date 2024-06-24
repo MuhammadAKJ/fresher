e18c482aaea059180dc9cdabaca4a1132f1a63c0
MuhammadAKJ <Muhammadakj11@gmail.com>
2024-06-24 21:05:53 +0100

    backup and recovery tools

--- install/sysadmin/app-backup.sh
+++ install/sysadmin/app-backup.sh
@@ -2,9 +2,14 @@
 
 # rsync
 # A fast and versatile file copying tool that can synchronize files and directories between different locations with minimal data transfer.
+sudo apt-get install rsync
 
 # Bacula
 # A set of programs that allow managing backup, recovery, and verification of data across a network of different kinds of computers.
+sudo apt install bacula #default bacula uses postgresql
+# sudo apt install bacula-director-sqlite3
+# sudo apt install bacula-director-mysql
 
 # Duplicity
 # A backup solution that provides encrypted, bandwidth-efficient backup using the rsync algorithm, with support for various storage backends.
+sudo apt install duplicity
